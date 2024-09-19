45dbafcaa6f11de7cb84d120c5012c47bb1c5ff4
Alex <alexkrieg@gmx.ch>
2024-09-19 17:45:26 +0200

    -

--- core/inc/RibbonWidget_debug.h
+++ core/inc/RibbonWidget_debug.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "RibbonWidget_global.h"
 
+/// USER_SECTION_START 14
+
+/// USER_SECTION_END
+
 // The Logger library is automaticly included if the logger dependency .cmake file is available
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	#include "Logger.h"
 #endif
 
@@ -107,7 +111,7 @@ namespace RibbonWidget
 /// USER_SECTION_END
 
 
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	class RIBBON_WIDGET_EXPORT Logger 
 	{
 		/// USER_SECTION_START 8
@@ -161,6 +165,9 @@ namespace RibbonWidget
 
 /// USER_SECTION_END
 #endif
+/// USER_SECTION_START 13
+
+/// USER_SECTION_END
 }
 
 
