38c682551bf4ce29305ae7930fd54712a8b80229
Alex Krieg <alexkrieg@gmx.ch>
2024-10-24 11:12:40 +0200

    -

--- RibbonWidget.cmake
+++ RibbonWidget.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     # Define the git repository and tag to download from
     set(LIB_NAME RibbonWidget)
-    set(LIB_MACRO_NAME RIBBON_WIDGET)
+    set(LIB_MACRO_NAME RIBBON_WIDGET_LIBRARY_AVAILABLE)
     set(GIT_REPO https://github.com/KROIA/RibbonWidget.git)
     set(GIT_TAG main)
 
