977567fd797e34a45b19d7eff6bd1766d1f27df9
Alex <alexkrieg@gmx.ch>
2026-03-26 22:19:08 +0100

    ~ Template version: 1.5.4

--- core/src/RibbonWidget_info.cpp
+++ core/src/RibbonWidget_info.cpp
@@ -167,9 +167,10 @@ namespace RibbonWidget
 		return widget;
 	}
 #else
-	QWidget* LibraryInfo::createInfoWidget(QWidget* parent)
+	QWidget* LibraryInfo::createInfoWidget(QWidget* parent, bool disableHyperlink)
 	{
 		RW_UNUSED(parent);
+		RW_UNUSED(disableHyperlink);
 		return nullptr;
 	}
 #endif
